2cfe65db538411d701a8593817dbb3cb0694c6e3
4piu <16668056+4piu@users.noreply.github.com>
2021-10-12 06:14:07 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,43 +4,31 @@
 # https://github.com/XTLS/Xray-core/discussions/59
 
 # transparent proxy
-#https://xtls.github.io/documents/level-2/tproxy/
+# https://xtls.github.io/Xray-docs-next/document/level-2/transparent_proxy/transparent_proxy.html
+# https://guide.v2fly.org/app/tproxy.html
 
-ip route add local default dev lo table 100 # 添加路由表 100
-ip rule add fwmark 1 table 100 # 为路由表 100 设定规则
+ip rule add fwmark 1 table 100 
+ip route add local 0.0.0.0/0 dev lo table 100
 
 iptables -t mangle -N XRAY
-iptables -t mangle -A XRAY -d 10.0.0.0/8 -j RETURN
-iptables -t mangle -A XRAY -d 100.64.0.0/10 -j RETURN
 iptables -t mangle -A XRAY -d 127.0.0.0/8 -j RETURN
-iptables -t mangle -A XRAY -d 169.254.0.0/16 -j RETURN
-iptables -t mangle -A XRAY -d 172.16.0.0/12 -j RETURN
-iptables -t mangle -A XRAY -d 192.0.0.0/24 -j RETURN
 iptables -t mangle -A XRAY -d 224.0.0.0/4 -j RETURN
-iptables -t mangle -A XRAY -d 240.0.0.0/4 -j RETURN
 iptables -t mangle -A XRAY -d 255.255.255.255/32 -j RETURN
-iptables -t mangle -A XRAY -d 192.168.0.0/16 -p tcp ! --dport 53 -j RETURN
 iptables -t mangle -A XRAY -d 192.168.0.0/16 -p udp ! --dport 53 -j RETURN
-iptables -t mangle -A XRAY -p tcp -j TPROXY --on-port $TPROXY_PORT --tproxy-mark 1
-iptables -t mangle -A XRAY -p udp -j TPROXY --on-port $TPROXY_PORT --tproxy-mark 1
+iptables -t mangle -A XRAY -d 192.168.0.0/16 -j RETURN
+iptables -t mangle -A XRAY -d 10.0.0.0/8 -p udp ! --dport 53 -j RETURN
+iptables -t mangle -A XRAY -d 10.0.0.0/8 -j RETURN
+iptables -t mangle -A XRAY -d 172.16.0.0/16 -p udp ! --dport 53 -j RETURN
+iptables -t mangle -A XRAY -d 172.16.0.0/16 -j RETURN
+iptables -t mangle -A XRAY -m mark --mark 0xff -j RETURN
+iptables -t mangle -A XRAY -p udp -j TPROXY --on-ip 127.0.0.1 --on-port 12345 --tproxy-mark 1
+iptables -t mangle -A XRAY -p tcp -j TPROXY --on-ip 127.0.0.1 --on-port 12345 --tproxy-mark 1
 iptables -t mangle -A PREROUTING -j XRAY
 
-iptables -t mangle -N XRAY_SELF
-iptables -t mangle -A XRAY_SELF -d 10.0.0.0/8 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 100.64.0.0/10 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 127.0.0.0/8 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 169.254.0.0/16 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 172.16.0.0/12 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 192.0.0.0/24 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 224.0.0.0/4 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 240.0.0.0/4 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 255.255.255.255/32 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 192.168.0.0/16 -p tcp ! --dport 53 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 192.168.0.0/16 -p udp ! --dport 53 -j RETURN
-iptables -t mangle -A XRAY_SELF -m mark --mark 2 -j RETURN
-iptables -t mangle -A XRAY_SELF -p tcp -j MARK --set-mark 1
-iptables -t mangle -A XRAY_SELF -p udp -j MARK --set-mark 1
-iptables -t mangle -A OUTPUT -j XRAY_SELF
+iptables -t mangle -N DIVERT
+iptables -t mangle -A DIVERT -j MARK --set-mark 1
+iptables -t mangle -A DIVERT -j ACCEPT
+iptables -t mangle -I PREROUTING -p tcp -m socket -j DIVERT
 
 
 # start xray
